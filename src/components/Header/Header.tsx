4de0b6d7ae61923de1392c5539d5819845adf8f7
Anna Rudas <rudas.annamaria@gmail.com>
2024-07-29 19:10:35 +0200

    fix header content and account dropdown bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,6 +13,7 @@ import { defaultTheme } from "../../constants";
 import { Link } from "react-router-dom";
 import UserIcon from "../../icons/UserIcon";
 import SignInIcon from "../../icons/SignInIcon";
+import { useLocation } from "react-router-dom";
 
 type HeaderProps = {
   isLandingPage?: boolean;
@@ -30,6 +31,10 @@ function Header({ isLandingPage, isErrorStyle }: HeaderProps) {
     isDropdownOpen,
     dropdownButtonRef,
   } = useContext(AppContext);
+
+  const location = useLocation();
+  const { pathname } = location;
+  const isDashboardPage = pathname === "/dashboard";
 
   const toggleTheme = () => {
     if (user) {
@@ -52,7 +57,9 @@ function Header({ isLandingPage, isErrorStyle }: HeaderProps) {
   return (
     <div
       {...className(
-        isLandingPage ? style.landingPageHeader : style.generalHeader,
+        isLandingPage || !isDashboardPage
+          ? style.landingPageHeader
+          : style.generalHeader,
         style.header
       )}
     >
@@ -81,30 +88,34 @@ function Header({ isLandingPage, isErrorStyle }: HeaderProps) {
       )}
       {user && !isErrorStyle && !isLandingPage && (
         <>
-          <div {...className(style.searchInputCon)}>
-            <SearchIcon {...className(style.searchIcon)} />
-            <input
-              {...className(style.searchInput)}
-              type="search"
-              placeholder="Search"
-              value={search}
-              onChange={(event) => setSearch(event.target.value)}
-            />
-          </div>
+          {isDashboardPage && (
+            <div {...className(style.searchInputCon)}>
+              <SearchIcon {...className(style.searchIcon)} />
+              <input
+                {...className(style.searchInput)}
+                type="search"
+                placeholder="Search"
+                value={search}
+                onChange={(event) => setSearch(event.target.value)}
+              />
+            </div>
+          )}
 
           <div {...className(style.setCon)}>
-            <button
-              disabled={isLoading}
-              {...className(style.iconBtn)}
-              onClick={toggleLayout}
-              title={user?.layout === "grid" ? "Grid view" : "List view"}
-            >
-              {user?.layout === "grid" ? (
-                <GridIcon {...className(style.viewIcon)} />
-              ) : (
-                <ListIcon {...className(style.viewIcon)} />
-              )}
-            </button>
+            {isDashboardPage && (
+              <button
+                disabled={isLoading}
+                {...className(style.iconBtn)}
+                onClick={toggleLayout}
+                title={user?.layout === "grid" ? "Grid view" : "List view"}
+              >
+                {user?.layout === "grid" ? (
+                  <GridIcon {...className(style.viewIcon)} />
+                ) : (
+                  <ListIcon {...className(style.viewIcon)} />
+                )}
+              </button>
+            )}
             <button
               disabled={isLoading}
               {...className(style.iconBtn)}
