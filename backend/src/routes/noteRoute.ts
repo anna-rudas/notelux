ef38b12668047e04829699583a94ad2588e4bf25
Anna Rudas <rudas.annamaria@gmail.com>
2025-03-30 17:41:49 +0200

    remove searchNotes controller and route

--- backend/src/routes/noteRoute.ts
+++ backend/src/routes/noteRoute.ts
@@ -7,6 +7,5 @@ router.get("/notes/:userId", noteController.getNotes);
 router.post("/notes", noteController.createNote);
 router.put("/notes/:id", noteController.updateNote);
 router.delete("/notes/:id", noteController.deleteNote);
-router.get("/notes/search", noteController.searchNotes);
 
 export default router;
