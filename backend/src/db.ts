181421667bc5a2d3fa462e51fd99f75aec8bab97
Anna Rudas <rudas.annamaria@gmail.com>
2025-03-30 18:39:57 +0200

    update database env variables

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,11 +5,11 @@ import { io } from "./index";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  database: process.env.DB_NAME,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: parseInt(process.env.PGPORT || "5432"),
+  database: process.env.POSTGRES_DB,
 });
 
 pool.on("error", (err) => {
