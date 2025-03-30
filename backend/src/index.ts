070e3ddcc93709988dd993cfded3315e9601f375
Anna Rudas <rudas.annamaria@gmail.com>
2025-03-30 19:37:36 +0200

    add cors options

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,12 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", noteRoute);
